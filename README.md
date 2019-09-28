59fe838585ce691cf8b1620bfff4835b92c2c4d1
Joseph Groark <joseph.groark@moneysupermarket.com>
2019-09-28 19:01:15 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-## Installation
+##Installation
 
 1. clone project
 2. cd ./VIO-DEMO
 3. npm install (expect a longer install)
 
-# Cypress
-from the command line type cypress open
-click on basic-demo.js
-1. checks for unintended css changed
-2. Has no detectable a11y violations on load
+#Cypress
+cypress open
+(opens cypress dashboard)
 
-# Percy
+cypress run
+(headless)
+
+npm test
+visual regression + accessibility
+
+#Percy
 Joey will sent you an invite to a project, here you will see the comparsion tool for visual regression working
 
-# CI 
+#CI 
 These can both be integrated easily into a build pipeline
 
--- cypress/integration/basic-demo.js
+++ cypress/integration/basic-demo.js
@@ -4,28 +4,10 @@ describe('Demo Visual Regression test', function () {
         cy.injectAxe()
     })
     it('checks for unintended css changes', function () {
-        cy.visit('https://demo.viosystems.com/enterUsername')
-        cy.get('#username')
-            .type('Jimbo')
-        cy.get('.button__submit-btn--next')
-            .click()
-        cy.url()
-            .should('include', '/ProfileDetails')
-        cy.percySnapshot()
+        cy.basicVisualRegression()
     })
 
     it('Has no detectable a11y violations on load (custom configuration)', () => {
-        // Configure aXe and test the page at initial load
-        cy.configureAxe({
-          branding: {
-            brand: String,
-            application: String
-          },
-          reporter: "option",
-          checks: [Object],
-          rules: [Object],
-          locale: Object
-        })
         cy.checkA11y()
-      })
+    })
 })
